8333bb94c8d00f8444e6a8398b48e022de00d047
Rawshan008 <pust.love008@gmail.com>
2019-01-29 01:34:42 +0600

    Javascript Array

---
+++ main.js
@@ -0,0 +1,50 @@
+// Array 
+// const newArray = [1, 2, 3];// array 
+// console.log(newArray);
+// array ar 4 ta method ase. push, pop, shidt, unshift
+/*
+Example One: Empty Array
+*/
+const myArray = [];
+console.log(myArray);
+// add element in the array
+myArray[0] = "first Element";
+console.log(myArray);
+myArray[1] = "Second Element";
+console.log(myArray);
+// akta Array ar aki index a bar bar declear korle array ar value change hobe
+// this type of array adding is not good
+// adding by method (push)
+myArray.push("Third Element")
+console.log(myArray);
+// array k dot notation dia show kora jai na .
+// console.log(myArray.0); //Uncaught SyntaxError: missing ) after argument list
+
+/*
+Push
+*/
+// array ar ses ar dike kono item add koraer jonno array.push use korte history.
+
+/*
+pop
+*/
+// array ar ses ar diker kono item remove korte chaile array.pop use korte hoi 
+
+/*
+shift
+*/
+// array ar suru ar diker kono item k remove korte array.shift use korte hoi.
+
+/*
+unshift
+*/
+// array ar suru ar diker kono item k add korte array.unshift use korte hoi.
+
+/*
+array ar kono item ar index no jante hobe arrName.indexOf('Karim');
+*/
+
+// ইন্ডেস্ক নাম্বার দিয়ে কোনো আইটেম রিমুভ করতেঃ
+
+// arrName.splice(1, 2);
+// এখানে ইনডেক্স নাম্বার কত থেকে শুরু করে কত পর্যন্ত রিমুভ করতে চাচ্ছেন সেগুলা আর্গুমেন্ট হিসেবে এখানে যাবে
