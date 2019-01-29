c31f7f8f8939fba02a352b36e5d1dca531a4e249
Rawshan008 <pust.love008@gmail.com>
2019-01-30 00:39:45 +0600

    Javascript Loop, for , while, do while

--- main.js
+++ main.js
@@ -1,50 +1,35 @@
-// Array 
-// const newArray = [1, 2, 3];// array 
-// console.log(newArray);
-// array ar 4 ta method ase. push, pop, shidt, unshift
-/*
-Example One: Empty Array
-*/
-const myArray = [];
-console.log(myArray);
-// add element in the array
-myArray[0] = "first Element";
-console.log(myArray);
-myArray[1] = "Second Element";
-console.log(myArray);
-// akta Array ar aki index a bar bar declear korle array ar value change hobe
-// this type of array adding is not good
-// adding by method (push)
-myArray.push("Third Element")
-console.log(myArray);
-// array k dot notation dia show kora jai na .
-// console.log(myArray.0); //Uncaught SyntaxError: missing ) after argument list
+// Javascript Loop
+// For loop
 
-/*
-Push
-*/
-// array ar ses ar dike kono item add koraer jonno array.push use korte history.
+for(var i=1; i<=10; i++){
+    console.log(i); // 1 2 3 4 5 6 7 8 10
+}
+console.log("----------");
+// define a array
+const myObjext = ["Rawshan", "ali", 25, true];
+for(var i=0; i<myObjext.length; i++){
+    console.log(myObjext[i]);
+}
+console.log("----While Loop----");
+var i = 0;
+while(i<5){
+    console.log(i);
+    i++;
+}
+console.log("----------");
+// print array by while loop
+var i = 0;
+while(i<myObjext.length){
+    console.log(myObjext[i]);
+    i++;
+}
 
-/*
-pop
-*/
-// array ar ses ar diker kono item remove korte chaile array.pop use korte hoi 
+// Do while loop
+console.log("--Do While loop--");
+// print array by do while loop
+var i = 0;
+do{
+    console.log(myObjext[i]);
+    i++;
+}while(i<myObjext.length);
 
-/*
-shift
-*/
-// array ar suru ar diker kono item k remove korte array.shift use korte hoi.
-
-/*
-unshift
-*/
-// array ar suru ar diker kono item k add korte array.unshift use korte hoi.
-
-/*
-array ar kono item ar index no jante hobe arrName.indexOf('Karim');
-*/
-
-// ইন্ডেস্ক নাম্বার দিয়ে কোনো আইটেম রিমুভ করতেঃ
-
-// arrName.splice(1, 2);
-// এখানে ইনডেক্স নাম্বার কত থেকে শুরু করে কত পর্যন্ত রিমুভ করতে চাচ্ছেন সেগুলা আর্গুমেন্ট হিসেবে এখানে যাবে
